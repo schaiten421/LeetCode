d77708f4820b2d04d490e9196f66ffb8e957f4d2
schaiten421 <128535434+schaiten421@users.noreply.github.com>
2023-07-13 12:54:41 +0530

    Time: 18 ms (81.84%), Space: 6.8 MB (97.51%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-    bool ispalindrome(string &s,int i,int j){
+    /*bool ispalindrome(string &s,int i,int j){
         int low=i,high=j;
         while(low<=high){
             if(s[low]!=s[high])return false;
             low++;high--;
         }
         return true;
+    }*/
+    int expand(int i,int j,string &s){
+        int left=i;int right=j;
+        while(left>=0 && right<s.length() && s[left]==s[right]){
+            left--;
+            right++;
+        }
+        return right-left-1;
     }
     string longestPalindrome(string s) {
-        int n=s.length(),start=0,len=0;
+        /*int n=s.length(),start=0,len=0;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 if(ispalindrome(s,i,j))
@@ -18,6 +26,25 @@ public:
                     }
             }
         }
-        return s.substr(start,len);
+        return s.substr(start,len);*/
+     int ans[]={0,0};
+        for(int i=0;i<s.length();i++){
+            int oddlength=expand(i,i,s);
+            if(oddlength>ans[1]-ans[0]+1){
+                int dist=oddlength/2;
+                ans[0]=i-dist;
+                ans[1]=i+dist;
+            }
+            int evenlength=expand(i,i+1,s);
+            if(evenlength>ans[1]-ans[0]+1){
+                int dist=(evenlength/2)-1;
+                ans[0]=i-dist;
+                ans[1]=i+1+dist;
+            }
+        }
+        int i=ans[0];
+        int j=ans[1];
+        return s.substr(i,j-i+1);
+        
     }
 };
