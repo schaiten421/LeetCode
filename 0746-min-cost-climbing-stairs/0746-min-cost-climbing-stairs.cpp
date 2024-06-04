6f58dab0cb72e63f6e30e9537de3101c4cea9e92
schaiten421 <128535434+schaiten421@users.noreply.github.com>
2024-06-04 22:52:35 +0530

    Time: 3 ms (79.84%), Space: 16.2 MB (83.73%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,18 +1,15 @@
 class Solution {
-    int f(int ind,vector<int> cost,vector<int>& dp){
-        if(ind==0 || ind==1)return cost[ind];
-        if(dp[ind]!=-1)return dp[ind];
-        return cost[ind]+min(f(ind-1,cost,dp),f(ind-2,cost,dp));
-    }
+    
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int> dp(n+1,0);
+        int prev=0,sec_prev=0;
         for(int i=2;i<=n;i++){
-            int onestep=dp[i-1]+cost[i-1];
-            int twostep=dp[i-2]+cost[i-2];
-            dp[i]=min(onestep,twostep);
+            int onestep=prev+cost[i-1];
+            int twostep=sec_prev+cost[i-2];
+            sec_prev=prev;
+            prev=min(onestep,twostep);
         }
-        return dp[n];
+        return prev;
     }
 };
