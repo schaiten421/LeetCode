42aaebff91937d7e705d7ccdf0be013b50754585
schaiten421 <128535434+schaiten421@users.noreply.github.com>
2024-06-06 13:37:16 +0530

    Time: 15 ms (35.98%), Space: 19.7 MB (59.16%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int f(int ind,int target,vector<int>& coins,vector<vector<int>>& dp){
-        if(ind==0){
-            if(target%coins[ind]==0)return 1;
-            return 0;
-        }
-        if(target==0)return 1;
-        if(dp[ind][target]!=-1)return dp[ind][target];
-        int nottake=f(ind-1,target,coins,dp);
-        int take=0;
-        if(coins[ind]<=target)take=f(ind,target-coins[ind],coins,dp);
-        return dp[ind][target]=take+nottake;
-        
-    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return f(n-1,amount,coins,dp);
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)dp[0][i]=1;
+        }
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=amount;j++){
+                int nottake=dp[i-1][j];
+                int take=0;
+                if(coins[i]<=j)take=dp[i][j-coins[i]];
+                dp[i][j]=take+nottake;
+            }
+        }
+        return dp[n-1][amount];
     }
 };
