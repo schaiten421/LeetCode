23964e877524bbed67e56ef6b861c3e8f45ea271
schaiten421 <128535434+schaiten421@users.noreply.github.com>
2024-06-06 11:06:19 +0530

    Time: 21 ms (80.73%), Space: 11.6 MB (96.28%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -13,13 +13,22 @@ private:
         return dp[ind][amount]=min(take,nottake);
     }
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans= f(n-1,amount,coins,dp);
-        if(ans>=1e9){
-            return -1;
+    int coinChange(vector<int>& coins, int n) {
+        // creating the base dp array, with first value set to 0
+        int dp[++n];
+        dp[0] = 0;
+        // more convenient to have the coins sorted
+        sort(begin(coins), end(coins));
+        // populating our dp array
+        for (int i = 1; i < n; i++) {
+            // setting dp[0] base value to 1, 0 for all the rest
+            dp[i] = INT_MAX;
+            for (int c: coins) {
+                if (i - c < 0) break;
+                // if it was a previously not reached cell, we do not add use it
+                if (dp[i - c] != INT_MAX) dp[i] = min(dp[i], 1 + dp[i - c]);
+            }
         }
-        else return ans;
+        return dp[--n] == INT_MAX ? -1 : dp[n];
     }
 };
