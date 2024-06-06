8229aebd7e403a3dd8804fa14ed3fdebeb800b08
schaiten421 <128535434+schaiten421@users.noreply.github.com>
2024-06-06 13:39:15 +0530

    Time: 11 ms (52.07%), Space: 19.7 MB (59.16%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -6,11 +6,8 @@ public:
         for(int i=0;i<=amount;i++){
             if(i%coins[0]==0)dp[0][i]=1;
         }
-        for(int i=0;i<n;i++){
-            dp[i][0]=1;
-        }
         for(int i=1;i<n;i++){
-            for(int j=1;j<=amount;j++){
+            for(int j=0;j<=amount;j++){
                 int nottake=dp[i-1][j];
                 int take=0;
                 if(coins[i]<=j)take=dp[i][j-coins[i]];
