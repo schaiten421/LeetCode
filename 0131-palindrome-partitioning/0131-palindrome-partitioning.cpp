b13ff71c6e650d291eaf02764b7d62fa7a8a2ea0
schaiten421 <128535434+schaiten421@users.noreply.github.com>
2024-06-13 18:35:05 +0530

    Time: 102 ms (34.98%), Space: 61.1 MB (44.38%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-      vector < vector < string > > res;
-      vector < string > path;
-      partitionHelper(0, s, path, res);
-      return res;
-    }
-
-  void partitionHelper(int index, string s, vector < string > & path,
-    vector < vector < string > > & res) {
-    if (index == s.size()) {
-      res.push_back(path);
-      return;
-    }
-    for (int i = index; i < s.size(); ++i) {
-      if (isPalindrome(s, index, i)) {
-        path.push_back(s.substr(index, i - index + 1));
-        partitionHelper(i + 1, s, path, res);
-        path.pop_back();
-      }
-    }
-  }
-
-  bool isPalindrome(string s, int start, int end) {
-    while (start <= end) {
-      if (s[start++] != s[end--])
-        return false;
+    bool isPalindrome(string String, int i, int j)
+    {
+    while (i < j) {
+        if (String[i] != String[j])
+            return false;
+        i++;
+        j--;
     }
     return true;
-  }
-    
+    }
+    void f(int ind,string s,string output,vector<string> &temp,vector<vector<string>> &ans){
+        if(ind==s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=ind;i<s.size();i++){
+            if(isPalindrome(s,ind,i)){
+                temp.push_back(s.substr(ind,i-ind+1));
+                f(i+1,s,output,temp,ans);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        string output="";
+        f(0,s,output,temp,ans);
+        return ans;
+        
+    }
 };
