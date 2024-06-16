07bdf415e8ddf49b411dbf251ba85341467d882d
schaiten421 <128535434+schaiten421@users.noreply.github.com>
2024-06-16 13:15:07 +0530

    Time: 368 ms (34.06%), Space: 64 MB (91.37%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<int> adj[n];
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+        }
+        vector<int> indegree(n);
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        vector<vector<int>> ans(n);
+        vector<int> toposort;
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0)q.push(i);
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            toposort.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(find(ans[it].begin(),ans[it].end(),node)==ans[it].end()){
+                    ans[it].push_back(node);
+                }
+                for(int i=0;i<ans[node].size();i++){
+                    if(find(ans[it].begin(),ans[it].end(),ans[node][i])==ans[it].end()){
+                        ans[it].push_back(ans[node][i]);
+                    }
+                }
+                if(indegree[it]==0)q.push(it);
+            }
+        }
+        for (int i = 0; i < n; i++)
+		{
+			sort(ans[i].begin(), ans[i].end());
+		}
+        return ans;
+        }
+};
+
