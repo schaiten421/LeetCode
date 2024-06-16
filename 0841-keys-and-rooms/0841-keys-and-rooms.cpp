498e0c8a58280da2ebb7c1a2617e098bf56b88e9
schaiten421 <128535434+schaiten421@users.noreply.github.com>
2024-06-16 21:55:29 +0530

    Time: 8 ms (43.19%), Space: 13.5 MB (74.24%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n=rooms.size();
+        vector<int> vis(n,0);
+        vis[0]=1;
+        queue<int> q;
+        q.push(0);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it:rooms[node]){
+                if(vis[it]==0){
+                    vis[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(vis[i]==0)return false;
+        }
+        return true;
+    }
+};
