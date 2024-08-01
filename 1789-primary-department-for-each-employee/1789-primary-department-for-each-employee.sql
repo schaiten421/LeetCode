7b7ff64ad80907456bf3fb646d3468b73a212e87
Chaitanya Sharma <128535434+schaiten421@users.noreply.github.com>
2024-08-01 23:25:09 +0530

    Time: 663 ms (18.16%), Space: 0B (100.00%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,6 @@
+SELECT employee_id, department_id FROM (
+SELECT *, COUNT(employee_id) OVER (PARTITION BY employee_id) AS EmployeeCount
+FROM Employee
+) EmployeePartition
+WHERE EmployeeCount=1
+OR primary_flag="Y";
